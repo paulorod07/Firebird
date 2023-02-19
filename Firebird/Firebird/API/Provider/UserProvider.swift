d491b9b3c587c773111462c82ff552e1211a4f05
Paulo Ãnes de Souza Rodrigues <pauloadesr@hotmail.com>
2023-02-19 17:01:47 -0300

    implementando camadas da api e método de login e registro firebase

---
+++ Firebird/Firebird/API/Provider/UserProvider.swift
@@ -0,0 +1,57 @@
+//
+//  UserProvider.swift
+//  Firebird
+//
+//  Created by Paulo Rodrigues on 19/02/23.
+//
+
+import Foundation
+import FirebaseAuth
+
+protocol UserProviderProtocol {
+    func register(
+        parameters: [AnyHashable:Any],
+        completionHandler: @escaping (Result<UserModel, Error>) -> Void
+    )
+    func login(
+        parameters: [AnyHashable:Any],
+        completionHandler: @escaping (Result<UserModel, Error>) -> Void
+    )
+}
+
+class UserProvider: UserProviderProtocol {
+    
+    lazy var auth: Auth = Auth.auth()
+    
+    func register(parameters: [AnyHashable : Any], completionHandler: @escaping (Result<UserModel, Error>) -> Void) {
+        guard
+            let body: NSDictionary = parameters[Constants.ParametersKeys.body] as? NSDictionary,
+            let userModel = body[Constants.ParametersKeys.userModel] as? UserModel
+        else { return }
+        
+        self.auth.createUser(withEmail: userModel.email, password: userModel.password) { result, error in
+            if let error = error {
+                completionHandler(.failure(error))
+            }
+            
+            completionHandler(.success(userModel))
+        }
+    }
+    
+    func login(parameters: [AnyHashable : Any], completionHandler: @escaping (Result<UserModel, Error>) -> Void) {
+        guard
+            let body: NSDictionary = parameters[Constants.ParametersKeys.body] as? NSDictionary,
+            let userModel = body[Constants.ParametersKeys.userModel] as? UserModel
+        else { return }
+        
+        self.auth.signIn(withEmail: userModel.email, password: userModel.password) { Result, error in
+            if let error = error {
+                completionHandler(.failure(error))
+            }
+            
+            completionHandler(.success(userModel))
+        }
+    }
+    
+    
+}
---
+++ Firebird/Firebird/Infrastructure/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Firebird
+//
+//  Created by Paulo Rodrigues on 19/02/23.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct ParametersKeys {
+        static let body = "body"
+        static let userModel = "userModel"
+    }
+    
+}
