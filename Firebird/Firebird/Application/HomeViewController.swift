24375e4f29af1b7358f4823f7dcc05a379ebde48
Paulo Ãnes de Souza Rodrigues <pauloadesr@hotmail.com>
2023-02-20 05:44:20 -0300

    Criando estrutura e telas no storyboard para fluxo MVC

--- Firebird/Firebird/Application/HomeViewController.swift
+++ Firebird/Firebird/Application/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Firebird
 //
 //  Created by Paulo Rodrigues on 19/02/23.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
