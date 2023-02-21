c7b5c03eb8b01cf8ac3021e29fca9d0dedfc7886
Paulo Ãnes de Souza Rodrigues <pauloadesr@hotmail.com>
2023-02-21 16:11:06 -0300

    Criando controller e ligações da tela de login (LoginViewController)

---
+++ MVP/Firebird/Firebird/Application/Login/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginViewController.swift
+//  Firebird
+//
+//  Created by Paulo Rodrigues on 21/02/23.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func registerButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+}
