90dbb5eb969e5e444c9af4a4dc1d1c73a508bc26
Paulo Ãnes de Souza Rodrigues <pauloadesr@hotmail.com>
2023-02-21 16:18:16 -0300

    Criando controller e ligações da tela de registro (RegisterViewController)

---
+++ MVP/Firebird/Firebird/Application/Register/RegisterViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RegisterViewController.swift
+//  Firebird
+//
+//  Created by Paulo Rodrigues on 21/02/23.
+//
+
+import Foundation
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func registerButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+}
